c174058c6dc099dc02b5c6de4cb71ed047fb9ba7
Dmay2000 <muwwal4@gmail.com>
2022-10-15 16:09:40 -0400

    Simply display user's inputs

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
-import express from 'express';
+const  express = require('express');
 const app = express();
-import { urlencoded } from 'body-parser';
+const bodyParcer = require('body-parser');
 
-app.use(urlencoded({extended:false}))
+app.use(bodyParcer.urlencoded({extended:false}))
 
-app.get('/', (req, res) => {
-    let name = req.body.Name;
-    let email =req.body.Email;
-    let subject = req.body.Subject;
-    let comment = req.body.Comment;
+app.post('/', (req, res) => {
+    let { body } = req;
+    let name = body.Name;
+    let email = body.Email;
+    let subject = body.Subject;
+    let comment = body.Comment;
     res.status (200);
-    res.end ( `${name, email, subject, comment}` )
-
-})
+    res.send( `<p> <h4> Username: ${name} </h4> </p> 
+               <p> <h4> Email: ${email } </p> 
+               <p> He said ${ comment } </p>` );
+});
 
 app.listen(3000);
